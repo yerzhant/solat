5f499cb4000ed6960ca5efd8fd96d2aa75a8850c
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2020-05-07 19:04:55 +0600

    Refactor solat widget

--- android/app/src/main/java/kz/azan/solat/SolatWidget.kt
+++ android/app/src/main/java/kz/azan/solat/SolatWidget.kt
@@ -6,7 +6,6 @@ import android.appwidget.AppWidgetProvider
 import android.content.Context
 import android.content.Intent
 import android.view.View
-import android.widget.LinearLayout
 import android.widget.RemoteViews
 
 class SolatWidget : AppWidgetProvider() {
@@ -25,27 +24,24 @@ class SolatWidget : AppWidgetProvider() {
         val views = RemoteViews(context.packageName, R.layout.solat_widget)
                 .apply {
                     setOnClickPendingIntent(R.id.solat_widget, mainActivityIntent)
+
+                    setTextViewText(R.id.widget_date, "12 Рамадан 1441")
+                    setTextViewText(R.id.widget_city, "Алматы")
+
+                    setTextViewText(R.id.fadjr, "04:08")
+                    setTextViewText(R.id.sunrise_active, "05:37")
+                    setTextViewText(R.id.dhuhr, "12:52")
+                    setTextViewText(R.id.asr, "17:53")
+                    setTextViewText(R.id.maghrib, "20:01")
+                    setTextViewText(R.id.isha, "21:31")
+
+                    setInt(R.id.sunrise_layout, "setBackgroundResource", R.drawable.active_time_background)
+                    setImageViewResource(R.id.sunrise_divider, R.drawable.active_time_divider)
+                    setViewVisibility(R.id.sunrise_label, View.GONE)
+                    setViewVisibility(R.id.sunrise_label_active, View.VISIBLE)
+                    setViewVisibility(R.id.sunrise, View.GONE)
+                    setViewVisibility(R.id.sunrise_active, View.VISIBLE)
                 }
-
-        views.apply {
-            setTextViewText(R.id.widget_date, "12 Рамадан 1441")
-            setTextViewText(R.id.widget_city, "Алматы")
-
-            setTextViewText(R.id.fadjr, "04:08")
-            setTextViewText(R.id.sunrise_active, "05:37")
-            setTextViewText(R.id.dhuhr, "12:52")
-            setTextViewText(R.id.asr, "17:53")
-            setTextViewText(R.id.maghrib, "20:01")
-            setTextViewText(R.id.isha, "21:31")
-
-            setInt(R.id.sunrise_layout, "setBackgroundResource", R.drawable.active_time_background)
-            setImageViewResource(R.id.sunrise_divider, R.drawable.active_time_divider)
-            setViewVisibility(R.id.sunrise_label, View.GONE)
-            setViewVisibility(R.id.sunrise_label_active, View.VISIBLE)
-            setViewVisibility(R.id.sunrise, View.GONE)
-            setViewVisibility(R.id.sunrise_active, View.VISIBLE)
-
-        }
 
         appWidgetManager.updateAppWidget(appWidgetId, views)
     }
