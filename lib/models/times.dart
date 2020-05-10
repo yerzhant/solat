7bd4d230d233a2cb5465e33f16bc20dd6f0627da
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2020-05-11 00:35:00 +0600

    Add main platform api

---
+++ lib/models/times.dart
@@ -0,0 +1,21 @@
+class Times {
+  final String dateByHidjra;
+  final String city;
+  final String fadjr;
+  final String sunrise;
+  final String dhuhr;
+  final String asr;
+  final String maghrib;
+  final String isha;
+
+  Times(
+    this.dateByHidjra,
+    this.city,
+    this.fadjr,
+    this.sunrise,
+    this.dhuhr,
+    this.asr,
+    this.maghrib,
+    this.isha,
+  );
+}
---
+++ lib/repositories/main_platform_api.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/services.dart';
+import 'package:solat/models/times.dart';
+
+class MainPlatformApi {
+  static const city = "city";
+  static const latitude = "latitude";
+  static const longitude = "longitude";
+
+  static const currentDateByHidjra = "currentDateByHidjra";
+
+  static const fadjr = 1;
+  static const sunrise = 2;
+  static const dhuhr = 3;
+  static const asr = 4;
+  static const maghrib = 5;
+  static const isha = 6;
+
+  static const azanFlags = "azanFlags";
+  static const azanFlagType = "azanFlagType";
+  static const azanFlagValue = "azanFlagValue";
+
+  static const channel = MethodChannel('solat.azan.kz/main');
+
+  Future<Times> getTodayTimes() async {
+    final result = await channel.invokeMapMethod("get-today-times");
+    return Times(
+      result[city],
+      result[currentDateByHidjra],
+      result[fadjr],
+      result[sunrise],
+      result[dhuhr],
+      result[asr],
+      result[maghrib],
+      result[isha],
+    );
+  }
+}
