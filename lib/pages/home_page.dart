2d7d1a58dc83c1f442887283b226149263b8fa0b
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2020-05-11 09:57:44 +0600

    Added background and logo to main page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:solat/blocs/settings/settings_bloc.dart';
 import 'package:solat/blocs/times/times_bloc.dart';
 import 'package:solat/pages/settings_page.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -21,7 +22,23 @@ class HomePage extends StatelessWidget {
       child: BlocBuilder<TimesBloc, TimesState>(
         builder: (context, state) {
           return Container(
-            child: Text('Main page'),
+            decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage('assets/images/masjid-0.jpg'),
+                fit: BoxFit.cover,
+              ),
+            ),
+            child: Padding(
+              padding: const EdgeInsets.only(top: 70),
+              child: Column(
+                children: <Widget>[
+                  GestureDetector(
+                    child: Image.asset('assets/images/logo.png'),
+                    onTap: () => launch('https://azan.kz'),
+                  )
+                ],
+              ),
+            ),
           );
         },
       ),
