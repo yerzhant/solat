fc9ba592442d4f872ea2d8c6008328a343b9160f
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2021-01-06 23:26:34 +0600

    Widen times widget

--- lib/consts.dart
+++ lib/consts.dart
@@ -1,7 +1,7 @@
 const primaryColor = 0xFF2F80ED;
 const primaryColor85 = 0xD82F80ED;
 
-const widgetWidth = 250.0;
+const widgetWidth = 270.0;
 const widgetItemHeight = 40.0;
 const widgetItemPadding = 15.0;
 const widgetMainFontSize = 16.0;
