e0f17e8888811d815b6882c41d9093193967a08b
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2022-01-19 13:11:22 +0600

    Add Hidjrah date

---
+++ ios/Runner/Utils/HijrahDate.swift
@@ -0,0 +1,17 @@
+//
+//  HijrahDate.swift
+//  Runner
+//
+//  Created by Yerzhan Tulepov on 19.01.2022.
+//
+
+func getCurrentHidrahDate() -> String {
+    let calendar = Calendar(identifier: Calendar.Identifier.islamicUmmAlQura)
+    
+    let formater = DateFormatter()
+    formater.calendar = calendar
+    formater.locale = Locale(identifier: "ru")
+    formater.dateFormat = "dd MMMM yyyy"
+    
+    return formater.string(from: Date())
+}
