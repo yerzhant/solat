f8c80b90db6e51e8e841ad2c730770a7f008ebeb
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2022-01-19 21:14:18 +0600

    Add alarm and notification services

--- ios/Runner/Alarm/Alarm.swift
+++ ios/Runner/Alarm/Alarm.swift
@@ -5,4 +5,42 @@
 //  Created by Yerzhan Tulepov on 19.01.2022.
 //
 
-import Foundation
+import BackgroundTasks
+
+enum AzanType : Int {
+    case fadjr = 1, sunrise, dhuhr, asr, maghrib, isha
+}
+
+struct AlarmService {
+    
+    private static let taskId = "kz.azan.solat.refresh"
+    
+    static func initialize() {
+        BGTaskScheduler.shared.register(forTaskWithIdentifier: taskId, using: nil) { task in
+            
+        }
+        
+        reschedule()
+    }
+    
+    private static func reschedule() {
+        let request = BGAppRefreshTaskRequest(identifier: taskId)
+        
+        var when = DateComponents()
+        when.minute = 5
+
+        request.earliestBeginDate = when.date
+        
+        do {
+            try BGTaskScheduler.shared.submit(request)
+        } catch {
+            print("Alarm rescheduling failed: \(error)")
+        }
+    }
+    
+    private static func handleRefresh(task: BGAppRefreshTask) {
+        reschedule()
+        
+        task.setTaskCompleted(success: true)
+    }
+}
--- ios/Runner/Alarm/Notification.swift
+++ ios/Runner/Alarm/Notification.swift
@@ -5,4 +5,58 @@
 //  Created by Yerzhan Tulepov on 19.01.2022.
 //
 
-import Foundation
+struct NotificationService {
+    
+    private static let center = UNUserNotificationCenter.current()
+    
+    static func initialize() {
+        requestAuthorization()
+    }
+    
+    private static func requestAuthorization() {
+        center.requestAuthorization(options: [.alert, .sound]) { _, error in
+            if let error = error {
+                print("User notification auth request error: \(error)")
+            }
+        }
+    }
+    
+    static func schedule(type: AzanType, time: String) {
+        center.getNotificationSettings { settings in
+            guard settings.authorizationStatus == .authorized else { return }
+            
+            let content = UNMutableNotificationContent()
+            content.body = time
+            content.sound = UNNotificationSound.default
+            switch type {
+            case .fadjr:
+                content.title = "Фаджр"
+            case .sunrise:
+                content.title = "Восход"
+            case .dhuhr:
+                content.title = "Зухр"
+            case .asr:
+                content.title = "Аср"
+            case .maghrib:
+                content.title = "Магриб"
+            case .isha:
+                content.title = "Иша"
+            }
+            
+            let timeParts = time.split(separator: ":")
+            var when = DateComponents()
+            when.hour = Int(timeParts[0])
+            when.minute = Int(timeParts[1])
+            
+            let trigger = UNCalendarNotificationTrigger(dateMatching: when, repeats: false)
+            
+            let request = UNNotificationRequest(identifier: "", content: content, trigger: trigger)
+            
+            center.add(request) { error in
+                if let error = error {
+                    print("Notification scheduling failed: \(error)")
+                }
+            }
+        }
+    }
+}
