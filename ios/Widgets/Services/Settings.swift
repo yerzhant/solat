4dea1e2e1aa219de2219be29bf5c84374b41f81d
Yerzhan Tulepov <yerzhan.tulepov@gmail.com>
2022-02-12 00:48:39 +0600

    Add/remove more services

---
+++ ios/Widgets/Services/Settings.swift
@@ -0,0 +1,23 @@
+//
+//  Settings.swift
+//  WidgetsExtension
+//
+//  Created by Yerzhan Tulepov on 12.02.2022.
+//
+
+import Foundation
+
+struct Settings {
+    private static let city = "city"
+    private static let requestHidjrahDateFromServer = "request-hijrah-date-from-server"
+    
+    private static let settings = UserDefaults.standard
+    
+    static func getCity() -> String? {
+        settings.string(forKey: city)
+    }
+    
+    static func getRequestHidjrahDateFromServer() -> Bool {
+        settings.bool(forKey: requestHidjrahDateFromServer)
+    }
+}
---
+++ ios/Widgets/Services/SolatTimes.swift
@@ -0,0 +1,33 @@
+//
+//  SolatTimes.swift
+//  WidgetsExtension
+//
+//  Created by Yerzhan Tulepov on 12.02.2022.
+//
+
+import Foundation
+
+struct SolatTimes {
+    
+    static func getForToday() async throws -> Times? {
+        let format = DateFormatter()
+        format.locale = Locale(identifier: "ru")
+        format.dateFormat = "dd-MM-yyyy"
+        let today = format.string(from: Date())
+        
+        if let times = try Database().find(on: today) {
+            return times
+//            return Times(date: "19-01-2022", fadjr: "12:22", sunrise: "12:22", dhuhr: "12:22", asr: "12:22", maghrib: "12:22", isha: "19:11")
+        }
+        
+        return nil
+    }
+    
+    static func getHijrahDate() async throws -> String {
+        if Settings.getRequestHidjrahDateFromServer() {
+            return try await AzanApi.getCurrentDateByHijrah()
+        } else {
+            return getCurrentHijrahDate()
+        }
+    }
+}
